63528976ea616edf16c1e79f46b629ef24a6424f
Will0mane <100951314+Will0mane@users.noreply.github.com>
2023-02-14 20:59:14 +0100

    Initial commit

---
+++ src/main/java/com/will0mane/apis/mathapi/MathAPI.java
@@ -0,0 +1,17 @@
+package com.will0mane.apis.mathapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * THIS IS ONLY USED TO PACKAGE THE
+ * API WITH A PLUGIN!
+ */
+public final class MathAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        //THIS IS ONLY USED TO PACKAGE THE
+        //API WITH A PLUGIN!
+
+    }
+}
---
+++ src/main/java/com/will0mane/apis/mathapi/api/MathUtils.java
@@ -0,0 +1,7 @@
+package com.will0mane.apis.mathapi.api;
+
+public class MathUtils {
+
+    
+
+}
